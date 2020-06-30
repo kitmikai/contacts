fb85873a48cf7fdf3e8c1a9cd6823e5a269c782e
kitmikai <matara.timothy@gmail.com>
2020-06-30 06:45:08 +0300

    Add styling for show-contact

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-function Contact(first, last){
+// business logic
+function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
+}
+
+Contact.prototype.fullName = function() {
+    return this.firstName + " " + this.lastName;
 }
 
 // user interface logic
@@ -18,4 +23,12 @@ $(document).ready(function() {
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
     });
+
+    $(".contact").last().click(function (){
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.firstName);
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+    });
+
 });
