4cb175bed78b656d8c5d2a68514b9ea3077aa191
kitmikai <matara.timothy@gmail.com>
2020-06-30 08:43:21 +0300

    Add click event listener

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ Contact.prototype.fullName = function() {
 
 // user interface logic
 $(document).ready(function() {
-    $("form#new-contact").submit(function(event){
+    $("form#new-contact").submit(function(event) {
         event.preventDefault();
 
         var inputtedFirstName = $("input#new-first-name").val();
@@ -18,17 +18,17 @@ $(document).ready(function() {
 
         var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-        $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+        $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
         $("input#new-first-name").val("");
         $("input#new-last-name").val("");
-    });
 
-    $(".contact").last().click(function (){
-        $("#show-contact").show();
-        $("#show-contact h2").text(newContact.firstName);
-        $(".first-name").text(newContact.firstName);
-        $(".last-name").text(newContact.lastName);
+        $(".contact").last().click(function (){
+            $("#show-contact").show();
+            $("#show-contact h2").text(newContact.firstName);
+            $(".first-name").text(newContact.firstName);
+            $(".last-name").text(newContact.lastName);
+        });
     });
 
 });
